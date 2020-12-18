5c5a758c65d88ff88e588b98bc1d4ee39cd4d7f6
Олеся Мартынюк <olesyamartinyuk@yandex.ru>
2020-12-19 00:47:39 +0300

    Settings view

---
+++ RSSReader/Resources/Theme/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  RSSReader
+//
+//  Created by Олеся Мартынюк on 18.12.2020.
+//  Copyright © 2020 Olesia Martinyuk. All rights reserved.
+//
+
+import UIKit
+
+class Theme {
+    static let accent = UIColor(named: "Accent")
+    static let background = UIColor(named: "Background")
+    static let tint = UIColor(named: "Tint")
+}
--- RSSReader/View Controllers/ArticlesTableViewController.swift
+++ RSSReader/View Controllers/ArticlesTableViewController.swift
@@ -23,7 +23,7 @@ class ArticlesTableViewController: UITableViewController
         super.viewDidLoad()
         self.tableView.isScrollEnabled = true
         self.refreshControl?.addTarget(self, action: #selector(refresh), for: UIControl.Event.valueChanged)
-        self.navigationController?.navigationBar.tintColor = .white
+//        self.navigationController?.navigationBar.tintColor = .white
         self.navigationController?.navigationBar.backgroundColor = .white
         fillingTableGaps()
     }
